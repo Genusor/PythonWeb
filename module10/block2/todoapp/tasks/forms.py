e60dc3869e6ba5e416e6243cb83af5f0c2f52dbc
genusor <genusor@gmail.com>
2019-05-21 02:37:07 +0300

    module10 block3

---
+++ module10/block2/todoapp/tasks/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+class AddTaskForm(forms.Form):
+    description = forms.CharField(max_length=64, label='')
+
+class TodoItemForm(forms.ModelForm):
+    class Meta:
+        model = TodoItem
+        fields = ('description',)
--- module10/block2/todoapp/tasks/views.py
+++ module10/block2/todoapp/tasks/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from tasks.models import TodoItem
+from tasks.forms import AddTaskForm
 
 def index(request):
     return HttpResponse("Примитивный ответ из приложения tasks")
@@ -14,10 +15,17 @@ def delete_task(request, uid):
     return redirect("/tasks/list")
 
 def task_create(request):
-    return render(
-        request,
-        'tasks/create.html',
-    )
+    if request.method == "POST":
+        form = AddTaskForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            desc = cd["description"]
+            t = TodoItem(description=desc)
+            t.save()
+            return redirect("/tasks/list")
+    else:
+        form = AddTaskForm()
+    return render(request, "tasks/create.html", {"form": form})
 
 def tasks_list(request):
     all_tasks = TodoItem.objects.all()
